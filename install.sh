bbf2157ddb97e16837e340768af4da932d17f978
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2024-08-23 13:35:19 +0100

    symlink fonts

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@
 [ ! -e ~/.config/helix ] && ln -s `pwd`/helix ~/.config/helix
 [ ! -e ~/.config/lazygit ] && ln -s `pwd`/lazygit ~/.config/lazygit
 [ ! -e ~/.config/wezterm ] && ln -s `pwd`/wezterm ~/.config/wezterm
+[ ! -e ~/.local/share/fonts ] && ln -s `pwd`/.local/share/fonts ~/.local/share/fonts
